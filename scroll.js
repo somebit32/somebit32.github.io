6f2950d55ca97c9b7f76beb005dc9a3639521f7d
Yuriy Anisimov <91047284+somebit32@users.noreply.github.com>
2023-03-08 17:35:18 +0300

    up

--- scroll.js
+++ scroll.js
@@ -1,13 +1,13 @@
 $('#right-button').click(function() {
   event.preventDefault();
-    $('.content').animate({
-      scrollLeft: "+=300px"
-    }, "slow");
+  $('.content').animate({
+    scrollLeft: "+=300px"
+  }, "slow");
 });
 
 $('#left-button').click(function() {
   event.preventDefault();
-    $('.content').animate({
-      scrollLeft: "-=300px"
-    }, "slow");
+  $('.content').animate({
+    scrollLeft: "-=300px"
+  }, "slow");
 });
